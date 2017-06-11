3d8e74d47b26bb7dadd67254affd1296b5c8d7d3
young <youngyang0820@gmail.com>
2017-06-11 17:11:30 +0800

    2017.6.11 41(Hard)

---
+++ H41_FirstMissingPositive.java
@@ -0,0 +1,24 @@
+/**
+ * Created by yangjiawei on 2017/6/11.
+ */
+public class H41_FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+
+        return nums.length + 1;
+    }
+
+    public void swap(int[] A, int i, int j) {
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
+}
