a8d5db991d73240c4a934a5104e22cdb30128606
young <youngyang0820@gmail.com>
2017-06-24 10:33:45 +0800

    2017.6.24 142(Medium)

---
+++ M142_LinkedListCycleII.java
@@ -0,0 +1,31 @@
+/**
+ * Created by yangjiawei on 2017/6/24.
+ */
+public class M142_LinkedListCycleII {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode slow2 = head;
+                while (slow != slow2) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
